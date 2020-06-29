bec3dfd05d1725ee586f21ddce658801a6b60f91
dubuholic <haezinny@gmail.com>
2020-06-30 00:42:52 +0900

    Update Profile at LikeInstaApp

--- Flutter/LikeInstaApp/lib/profile.dart
+++ Flutter/LikeInstaApp/lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class _ProfileState extends State<Profile> with AutomaticKeepAliveClientMixin<Profile> {
   @override 
@@ -15,9 +16,7 @@ class _ProfileState extends State<Profile> with AutomaticKeepAliveClientMixin<Pr
         backgroundColor: Colors.black,
       ),
       body: Center(
-        child: Text('Content of Profile',
-        style: TextStyle(fontSize: 30)
-        ),
+        child: LoginScreen(),
       ),
     );
   }
@@ -30,3 +29,85 @@ class Profile extends StatefulWidget {
   _ProfileState createState() => _ProfileState();
 }
 
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+
+  String id = '';
+  String password ='';
+
+  @override 
+  Widget build(BuildContext context) {
+    return Container( 
+      alignment: Alignment.center,
+      child: Form(
+        key: formKey,
+        child: Column(
+          children: <Widget>[
+            Container(margin: EdgeInsets.only(top: 200.0)),
+            Text('Like Insta'),
+            Container(margin: EdgeInsets.only(top: 100.0)),
+            idField(),
+            passwordField(),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget idField() {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+        labelText: 'ID',
+        hintText: 'phone number, user name or email',
+        fillColor: Colors.black,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: BorderSide(
+            color: Colors.blue,
+          ),
+        ),
+      ),
+      onSaved: (String value) {
+        id = value;
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+        fillColor: Colors.black,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: BorderSide(
+            color: Colors.blue,
+          ),
+        ),
+      ),
+
+      onSaved: (String value) {
+        password = value;
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      color: Colors.lightBlue,
+      child: Text('Sign In'),
+      onPressed: () {
+        formKey.currentState.save();
+      },      
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override 
+  _LoginScreenState createState() => _LoginScreenState();
+}
